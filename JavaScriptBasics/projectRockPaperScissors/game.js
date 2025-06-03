7333fd22cd25e265514a74dc32fe3a724a8019e3
Iara Flaminio <136760650+iaraflaminio@users.noreply.github.com>
2025-06-02 23:18:29 -0300

    feat: add playRound function to compare choices and update scores

--- JavaScriptBasics/projectRockPaperScissors/game.js
+++ JavaScriptBasics/projectRockPaperScissors/game.js
@@ -16,3 +16,22 @@ function getHumanChoice() {
     const userChoice = prompt("Choose rock, paper, or scissors:");
     return userChoice.toLowerCase()
 }
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!")
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        humanScore++
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`)
+    } else {
+        computerScore++
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+    }
+}
+
+const humanSelection = getHumanChoice()
+const computerSelection = getComputerChoice()
